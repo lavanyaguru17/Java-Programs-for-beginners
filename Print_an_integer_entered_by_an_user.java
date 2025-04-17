b349ad8d860ba62f41e7794157aa843b247d9d0b
lavanyaguru17 <lavanyaguru17@gmail.com>
2025-04-17 08:30:31 +0530

    first

---
+++ Print_an_integer_entered_by_an_user.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Print_an_integer_entered_by_an_user{
+    public static void main(String args[]){
+        Scanner reader=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number=reader.nextInt();
+        System.out.print("You entered number: "+number);
+    }
+}
--- Print_an_integers_entered_by_an_user.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Print_an_integers_entered_by_an_user{
-    public static void main(String args[]){
-        Scanner reader=new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number=reader.nextInt();
-        System.out.print("You entered number: "+number);
-    }
-}
