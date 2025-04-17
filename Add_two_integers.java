ffc83c52c82e34a7e6ca62cb04f4a3b41875cfb3
lavanyaguru17 <lavanyaguru17@gmail.com>
2025-04-17 09:43:51 +0530

    first1

--- Add_two_integers.java
+++ Add_two_integers.java
@@ -1,23 +1,25 @@
 //compile time
-class Add_two_integers{
-    public static void main(String[] args){
-        int first=10;
-        int second=20;
-        int result=first+second;
-        System.out.print(first+" + "+second+" = "+result);
+class Add_two_integers {
+
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 20;
+        int result = first + second;
+        System.out.print(first + " + " + second + " = " + result);
     }
 }
 
 //run time
 import java.util.Scanner;
-class Add_two_integers{
-    public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
+class Add_two_integers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the first number:");
-        int first=scanner.nextInt();
+        int first = scanner.nextInt();
         System.out.print("Enter the second number:");
-        int second=scanner.nextInt();
-        int result=first+second;
-        System.out.println(first+" + "+second+" = "+result);
+        int second = scanner.nextInt();
+        int result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
     }
 }
---
+++ Check_whether_a_number_is_ever_or_odd.java
@@ -0,0 +1,24 @@
+//compile time
+class Check_whether_a_number_is_ever_or_odd{
+    public static void main(String[] args){
+        int num=7;
+        if(num%2==0){
+            System.out.println("The number "+num+" is even");
+        }
+        else{
+            System.out.println("The number "+num+" is odd");
+        }
+    }
+}
+
+//run time
+import java.util.Scanner;
+class Check_whether_a_number_is_ever_or_odd{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int num=scanner.nextInt();
+        String result=(num%2==0)?"even":"odd";
+        System.out.println(String.format("The number %d is %s",num,result));
+    }
+}
